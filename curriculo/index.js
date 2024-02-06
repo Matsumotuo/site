926fa16651f5e21423bfeebaf90ec14eab18692b
João Vitor Matsumoto <141677227+Matsumotuo@users.noreply.github.com>
2024-02-06 10:36:57 -0300

    Delete curriculo directory

--- curriculo/index.js
+++
@@ -1,7 +0,0 @@
-function toggleSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    section.classList.toggle('open');
-    section.classList.toggle('closed');
-    var content = section.querySelector('ul') || section.querySelector('p');
-    content.classList.toggle('hidden');
-}
