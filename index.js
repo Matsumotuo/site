8cb865b77574375ac25bbe62f1af59905f9c3f08
Matsumotuo <zmatsu000@gmail.com>
2024-02-27 16:15:02 -0300

    Mudei tudo kkkk

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-function toggleSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    section.classList.toggle('open');
-    section.classList.toggle('closed');
-    var content = section.querySelector('ul') || section.querySelector('p');
-    content.classList.toggle('hidden');
+const text = document.querySelector(".segundo-text");
 
+const textLoad= () => {
+    setTimeout(() => {
+        text.textContent = "Bem vindo!";
+    },0);
+    setTimeout(() => {
+        text.textContent = "Sou o João!";
+    },4000);
 }
-
-let currentSlide = 0;
-const slides = document.querySelectorAll('.container img');
-
-function nextSlide() {
-    slides[currentSlide].style.display = 'none';
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].style.display = 'block';
-}
-
-setInterval(nextSlide, 2500);
-
+textLoad(0);
+setInterval(textLoad,8000);
