35d3ee48f5a2b705b63eb0d01095c8af134ef598
Matsumotuo <zmatsu000@gmail.com>
2024-02-28 14:29:31 -0300

    segunda pagina

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ const textLoad= () => {
 }
 textLoad(0);
 setInterval(textLoad,8000);
+
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
